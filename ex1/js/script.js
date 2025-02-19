c5482acf47a760ad27621c0a3093d03679d0cf82
rramalho <raphael.athenesi@gmail.com>
2025-02-19 12:09:46 -0300

    correcao

--- ex1/js/script.js
+++ ex1/js/script.js
@@ -1,4 +1,5 @@
 n1 = parseFloat(prompt('Digite o valor da doação em dolar'));
+
 res = n1 * 5;
 
 alert(`A conversao fica: ${res} !`);
--- ex2/js/script.js
+++ ex2/js/script.js
@@ -1,7 +1,7 @@
-inicio =parseFloat(prompt(`Digite o KM mostrado no painel:`))
+inicio = parseFloat(prompt(`Digite o KM mostrado no painel:`))
 posto = parseFloat(prompt(`Digite os KM ao chegar no posto de gasolina:`))
-abastecidos = parseFloat(prompt(`Digite quantos litros foi reabastecidos:`))
+abastecido = parseFloat(prompt(`Digite quantos litros foi reabastecidos:`))
 
-media = inicio + posto / abastecidos;
+kmLitro = (posto - inicio) / abastecido;
 
-alert(`O veiculo faz ${media}km/l`)
+alert(`O veiculo faz ${kmLitro.toFixed(3)}km/l`)
