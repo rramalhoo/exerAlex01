7d78a643242c1896adcfcec3aa01fbff488387ee
rramalho <raphael.athenesi@gmail.com>
2025-02-19 12:09:51 -0300

    correcao

--- ex3/js/script.js
+++ ex3/js/script.js
@@ -2,6 +2,6 @@ ac = parseFloat(prompt('Digite a nota da AC'));
 ag = parseFloat(prompt('Digite a nota da Avaliacao Geral'));
 at = parseFloat(prompt('Digite a nota da Avaliação Trimestral'));
 
-mediaGeral = (ac * 20 + ag * 30 + at * 70) / 100;
+mediaGeral = ((ac * 20) + (ag * 30) + (at * 70)) / 100;
 
 alert(`A media é ${mediaGeral}`)
